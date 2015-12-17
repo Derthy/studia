65386bb4e95017e482eb74a5f7d3d3c704f37ba5
Dawid Szubert <dawid.szubert@gft.com>
2015-12-17 18:48:46 +0000

    zaktualizowany update_location

--- PROJEKT/update_location.php
+++ PROJEKT/update_location.php
@@ -1,61 +1,38 @@
 <?php
-require_once __DIR__ . '/db_connect.php';
-
+ 
 $response = array();
 
-$db = new DB_CONNECT();
-if (!$db){
+$data = json_decode(file_get_contents('php://input'),true); 
+
+if ($data){
+
+    $longtitude = $data['longtitude'];
+    $latitude = $data['latitude'];
+    $mpk_nr = $data['mpk_nr'];
+    $line = $data['line'];
+
+    require_once __DIR__ . '/db_connect.php';
+ 
+    $db = new DB_CONNECT();
+ 
+    mysql_query("UPDATE sample set longtitude='$longtitude',latitude='$latitude' where mpk_nr=$mpk_nr and line=$line");
+
+    if (mysql_affected_rows() > 0) {
+        $response["success"] = 1;
+        $response["message"] = "Location successfully updated.";
+        mysql_query("COMMIT"); 
+
+        echo json_encode($response);
+    } else {
+        $response["success"] = 0;
+        $response["message"] = "0 rows updated";
+
+        echo json_encode($response);
+    }
+} else {
     $response["success"] = 0;
-    $response["message"] = "couldn't connect to db";
+    $response["message"] = "no json found";
  
     echo json_encode($response);
 }
-
-
-if ( isset($_POST['xval']) && isset($_POST['yval'])) {
-    $xval = $_POST['xval'];
-    $yval = $_POST['yval'];
- 
-    $result = mysql_query("update sample set XVAL='$xval',YVAL='$yval'");
-
-    if ($result) {
-        $response["success"] = 1;
-        $response["message"] = "Location successfully updated.";
- 
-        echo json_encode($response);
-    } else {
-        $response["success"] = 0;
-        $response["message"] = "Oops! An error occurred.";
- 
-        echo json_encode($response);
-    }
-}
-else {
-
-$result = mysql_query("SELECT * FROM sample WHERE PKEY = 1");
-
-    if (mysql_num_rows($result) > 0) {
-
-        $result = mysql_fetch_array($result);
-
-        $location = array();
-        $location["xval"] = $result["XVAL"];
-        $location["yval"] = $result["YVAL"];
-        $location["last_updated"] = $result["LAST_UPDATED"];
-
-        $response["success"] = 1;
-        $response["location"] = array();
-
-        array_push($response["location"], $location);
-
-        echo json_encode($response);
-    } else {
-        $response["success"] = 0;
-        $response["message"] = "No records found";
-
-        echo json_encode($response);
-    } 
-}
-
-
 ?>
